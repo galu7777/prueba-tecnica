582db46ea3a6da8e351240d0cbc7a633ec55c007
Luis Galea <luisgalea31@gmail.com>
2024-03-15 08:07:17 -0400

    feat<prueba-tecnica/client/src/types/index.ts>: create and config file

---
+++ prueba-tecnica/client/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Customer {
+    id: number;
+    customerId: string;
+    firstName: string;
+    lastName: string;
+    company: string;
+    city: string;
+    country: string;
+    phone1: string;
+    phone2: string;
+    email: string;
+    subscriptionDate: Date;
+    website: string;
+}
