341908cf44a765d1420df16ab4f075d77f1524e5
Luis Galea <luisgalea31@gmail.com>
2024-03-15 11:58:05 -0400

    fix<prueba-tecnioca/client/src/views>: fix styles btn

--- prueba-tecnica/client/src/views/ViewAllCustomers.tsx
+++ prueba-tecnica/client/src/views/ViewAllCustomers.tsx
@@ -71,7 +71,7 @@ export function ViewAllCustomers() {
             {!initialLoad && (
             <button 
                 onClick={handleInitialLoad} 
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                className="bg-btn text-white px-4 py-2 rounded-md"
             >
                 Load Data
             </button>
