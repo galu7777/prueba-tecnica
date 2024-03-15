57269e0ed3b5d76787cfdfd5fed661c13a059069
Luis Galea <luisgalea31@gmail.com>
2024-03-15 10:11:16 -0400

    feat<prueba-tecnica/client/src/actions/fetch-customer.ts> add func fetchsearchcustomer

--- prueba-tecnica/client/src/actions/fetch-customers.ts
+++ prueba-tecnica/client/src/actions/fetch-customers.ts
@@ -4,13 +4,28 @@ export async function fetchCustomers(page: number): Promise<Customer[] | null> {
     const apiUrl = `http://localhost:3001/v1/customers/get_all_customer?page=${page}`;
     try {
         const response = await fetch(apiUrl);
-        console.log("res: ", response)
         if (!response.ok) {
             throw new Error(`Error fetching data: ${response.statusText}`);
         }
 
         const data = await response.json();
-        console.log("data: ", data)
+        return data as Customer[];
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error; // Propagate the error for better error handling upstream
+    }
+}
+
+export async function fetchSearchCustomers(searchTerm: string, page: number): Promise<Customer[] | null> {
+    const apiUrl = `http://localhost:3001/v1/customers/search_customer?searchTerm=${searchTerm}&page=${page}&limit=10`;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Error fetching data: ${response.statusText}`);
+        }
+        
+        const data = await response.json();
+        console.log(data)
         return data as Customer[];
     } catch (error) {
         console.error("Error fetching data:", error);
