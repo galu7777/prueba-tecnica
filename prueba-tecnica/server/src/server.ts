cb44c08c4094cd700866366c94ecbfd6eed239b7
Luis Galea <luisgalea31@gmail.com>
2024-03-15 00:24:14 -0400

    feat<prueba-tecnica/server/src/server.ts>: create and configure file

---
+++ prueba-tecnica/server/src/server.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response, NextFunction } from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import routes from "./routes/index";
+
+require('./db.ts');
+
+const server = express();
+
+// Middleware para establecer el nombre del servidor
+server.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.serverName = 'API';
+    next();
+});
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+server.use('/', routes);
+
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+
+export default server;
