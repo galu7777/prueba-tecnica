b0f0aae1852a0fd031f69d9301ea8a8e5869ece2
Luis Galea <luisgalea31@gmail.com>
2024-03-15 08:06:54 -0400

    feat<prueba-tecnica/client/src/app/page.tsx>: import lore-more

--- prueba-tecnica/client/src/app/page.tsx
+++ prueba-tecnica/client/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { LoadMore } from "@/components/lore-more";
+
 export default function Home() {
-	return <main>🚀🚀🚀</main>;
+	return (
+		<div className="w-full h-screen flex justify-center items-center flex-col">
+		<h1>Scroll Infinity</h1>
+		<div className="w-[85%] h-85vh bg-gray-700 overflow-y-auto">
+			<LoadMore />
+		</div>
+		</div>
+	);
 }
