70faf1a6adfb31ef329de4c6228eceeb64b1c9c0
Luis Galea <luisgalea31@gmail.com>
2024-03-15 10:12:14 -0400

    feat<prueba-tecnica/client/src/app/page.tsx> add styles and navigation

--- prueba-tecnica/client/src/app/page.tsx
+++ prueba-tecnica/client/src/app/page.tsx
@@ -1,12 +1,42 @@
-import { LoadMore } from "@/components/lore-more";
+"use client"
+
+import { useState } from 'react';
+import { ViewAllCustomers } from "@/views/ViewAllCustomers";
+import { ViewSearch } from "@/views/viewSearch";
 
 export default function Home() {
-	return (
-		<div className="w-full h-screen flex justify-center items-center flex-col">
-		<h1>Scroll Infinity</h1>
-		<div className="w-[85%] h-85vh bg-gray-700 overflow-y-auto">
-			<LoadMore />
-		</div>
-		</div>
-	);
+    const [currentView, setCurrentView] = useState('all'); // Estado para mantener la vista actual
+
+    const handleViewAllClick = () => {
+        setCurrentView('all'); // Cambiar a vista de todos los clientes
+    };
+
+    const handleSearchClick = () => {
+        setCurrentView('search'); // Cambiar a vista de búsqueda
+    };
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center flex-col">
+            <div className="flex items-center mb-4">
+                <img src="https://netsocs.com/logo-netsocs-03.png" alt="NetSocs Logo" className="h-8 mr-2" />
+                <h1 className="text-3xl font-bold">Prueba técnica Netsocs</h1>
+            </div>
+            <div className="w-[85%] h-85vh bg-gray-700 overflow-y-auto">
+                {currentView === 'all' && <ViewAllCustomers />}
+                {currentView === 'search' && <ViewSearch />}
+            </div>
+
+            <div className="flex justify-center mt-4">
+                <button 
+                    onClick={handleViewAllClick} 
+                    className="bg-green-500 text-white px-4 py-2 rounded-md mr-4"
+                >Ver Todos</button>
+                <button 
+                    onClick={handleSearchClick}
+                    className="bg-green-500 text-white px-4 py-2 rounded-md"
+                >Buscar</button>
+            </div>
+        </div>
+    );
 }
+
